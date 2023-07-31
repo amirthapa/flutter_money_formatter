7d49cfdaae270c2dbfee8a8653ba602e9628107c
amirthapa <aameer.thapa1@gmail.com>
2023-07-31 20:45:56 +0545

    fix: Decrease version to intl 17

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/wisdomekeh/dev/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/wisdomekeh/workspace/flutter/flutter_money_formatter/example"
+export "FLUTTER_ROOT=/Users/venillam1/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/venillam1/Documents/virtulaamir/flutter_money_formatter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/venillam1/Documents/virtulaamir/flutter_money_formatter/example/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/venillam1/Documents/virtulaamir/flutter_money_formatter/example/.dart_tool/package_config.json"
